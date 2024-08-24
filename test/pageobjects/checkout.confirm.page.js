448d2de82c422c04b81afd2fd979068d0e6b0766
Artem Perehonchuk <artem.perehonchuk@gmail.com>
2024-08-24 17:25:47 +0300

    project uses commonJS

--- test/pageobjects/checkout.confirm.page.js
+++
@@ -1,7 +0,0 @@
-import Page from "./page.js";
-
-class CheckoutConfirmPage extends Page {
-    get finishBtn () {return $('#finish');}
-}
-
-export default CheckoutConfirmPage;
--- test/pageobjects/product.page.js
+++
@@ -1,7 +0,0 @@
-import Page from "./page.js";
-
-class ProductPage extends Page {
-    get backToProductsBtn () {return $('#back-to-products');}
-}
-
-export default ProductPage;
