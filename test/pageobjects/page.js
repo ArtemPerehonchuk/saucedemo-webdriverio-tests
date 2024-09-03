32dd504f8dbf7c9ced2f64c1f170abaa76dfa987
Artem Perehonchuk <artem.perehonchuk@gmail.com>
2024-09-03 14:52:31 +0300

    Update page.js

--- test/pageobjects/page.js
+++ test/pageobjects/page.js
@@ -6,21 +6,6 @@ class Page {
     async checkBrowserUrl(expectedUrl) {
         await expect(browser).toHaveUrl(expectedUrl);
     }
-
-    // async clickOnElement(element) {
-    //     await element.waitForEnabled({timeot: 5000});
-    //     await element.click();
-    //     await browser.pause(2000);
-    // }
-
-    // async scrollToElement(element) {
-    //     await element.click();
-    // }
-
-    // async checkElementIsDisplayed(element) {
-    //     await expect(await element.isDisplayed()).toBe(true);
-    // }
-
 }
 
 export default Page;
